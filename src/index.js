619eaa27fc984f937c7c80a928435d2e4af11db9
rajat <rajat2411gupta@gmail.com>
2022-02-14 23:04:18 +0530

    delete  files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-require("file-loader?name=[name].[ext]!./index.html")
-import React from "react";
-import { render } from "react-dom";
-import App from "./App.jsx";
-
-render(<App/>, document.getElementById("root"));
